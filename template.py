9c278051e742a7c06722849fe7decb61708d704a
James Sue-Ling <james_sueling@hotmail.co.uk>
2023-12-08 15:43:17 +0000

    add template

---
+++ template.py
@@ -0,0 +1,17 @@
+class Day:
+
+  def __init__(self):
+    with open("input.txt", "r") as f:
+      self.input = f.read().split("\n")
+    with open("example.txt", "r") as f:
+      self.example = f.read().split("\n")
+  
+  def p1(self):
+    return
+
+  def p2(self):
+    return
+
+d = Day()
+d.p1()
+d.p2()
